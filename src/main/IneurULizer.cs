3e1d421a0599c175ca3defc858b4b96309a6ffb0
elmerbool <35117496+elmerbool@users.noreply.github.com>
2024-07-09 00:21:46 +1000

    Added neurULizer and options.

---
+++ src/main/IneurULizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Coding
+{
+    public interface IneurULizer
+    {
+        Task<Ensemble> neurULizeAsync<TValue>(TValue value, IneurULizationOptions options);
+    }
+}
